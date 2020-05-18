7110bee31cf6400422a559812590a6ec71442b43
Keelana Char <keelanachar@gmail.com>
2020-05-17 19:54:49 -0500

    Add if/else statement to prevent hands from transitioning backwards.

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -1,13 +1,13 @@
 const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+const allHands = document.querySelectorAll('.hand');
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(seconds);
 
   const minute = now.getMinutes();
   const minDegrees = ((minute / 60) * 360) + 90;
@@ -16,6 +16,12 @@ function setDate() {
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  if (secondsDegrees === 90) {
+    allHands.forEach(hand => hand.style.transition = 'none');
+  } else {
+    allHands.forEach(hand => hand.style.transition = '');
+  }
 }
 
 setInterval(setDate, 1000);
