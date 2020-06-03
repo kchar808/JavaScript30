d3e02594fe6be28aa1fd74b3e55dc0e5be74405f
Keelana Char <keelanachar@gmail.com>
2020-06-02 21:34:52 -0500

    Added greenScreen function. Completed Day 19.

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,8 +29,10 @@ function paintToCanvas() {
     // mess with them
     // pixels = redEffect(pixels);
 
-    pixels = rgbSplit(pixels);
-    ctx.globalAlpha = 0.1;
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+
+    pixels = greenScreen(pixels);
     // put them back
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -65,6 +67,38 @@ function rgbSplit(pixels) {
     pixels.data[i + 500] = pixels.data[i + 1]; // green
     pixels.data[i - 550] = pixels.data[i + 2];// blue
   }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  // Iterate through a NodeList of all input elements within the div with class rgb 
+  // and set a key in the levels object as a given input's name property, 
+  // and set the value to be the given input's value.
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  // Update the values of the pixels in the image so that we remove all RGB values
+  // that are within the range defined by the user using range elemets in the page 
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
   return pixels;
 }
 
